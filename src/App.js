80aeef65f1f6fcb36e2a0b762e33c26c1a6dc49d
shahriad00 <shahriad555@gmail.com>
2021-09-03 16:59:04 +0600

    animation added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useRef } from "react";
 import "./App.css";
-import { Canvas, useFrame } from "react-three-fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, softShadows, Circle } from "@react-three/drei";
+
+softShadows();
 
 const Box = () => {
     const mesh = useRef(null);
     useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
     return (
-        <mesh ref={mesh}>
+        <mesh castShadow ref={mesh}>
             <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-            <meshStandardMaterial attach="material" color="black" />
+            <meshStandardMaterial attach="material" color="blue" />
         </mesh>
     );
 };
@@ -17,11 +19,41 @@ const Box = () => {
 function App() {
     return (
         <>
-            <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 50 }}>
-                <ambientLight intensity={1} />
+            <Canvas
+                shadowMap
+                colorManagement
+                camera={{ position: [-5, 2, 10], fov: 50 }}
+            >
+                <ambientLight intensity={0.3} />
+                <directionalLight
+                    castShadow
+                    position={[0, 10, 0]}
+                    intensity={1.5}
+                    shadow-mapSize-width={1024}
+                    shadow-mapSize-height={1024}
+                    shadow-camera-far={50}
+                    shadow-camera-left={-10}
+                    shadow-camera-right={10}
+                    shadow-camera-top={10}
+                    shadowCameraBottom={-10}
+                />
                 <pointLight position={[-10, 0, -20]} intensity={1} />
                 <pointLight position={[0, -20, 0]} intensity={2} />
+                <group>
+                    <mesh
+                        receiveShadow
+                        rotation={[-Math.PI / 2, 0, 0]}
+                        position={[0, -3, 0]}
+                    >
+                        <planeBufferGeometry
+                            attach="geometry"
+                            args={[100, 100]}
+                        />
+                        <shadowMaterial attach="material" />
+                    </mesh>
+                </group>
                 <Box />
+                <Circle position={[0, -2, 0]} />
                 <OrbitControls />
             </Canvas>
         </>
