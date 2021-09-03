701c42baad511140120912aa34171ec583a3ef19
shahriad00 <shahriad555@gmail.com>
2021-09-03 15:46:34 +0600

    animation added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
+import { Canvas, useFrame } from "react-three-fiber";
+import { OrbitControls } from "@react-three/drei";
+
+const Box = () => {
+    const mesh = useRef(null);
+    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+    return (
+        <mesh ref={mesh}>
+            <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+            <meshStandardMaterial attach="material" color="black" />
+        </mesh>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 50 }}>
+                <ambientLight intensity={1} />
+                <pointLight position={[-10, 0, -20]} intensity={1} />
+                <pointLight position={[0, -20, 0]} intensity={2} />
+                <Box />
+                <OrbitControls />
+            </Canvas>
+        </>
+    );
 }
 
 export default App;
